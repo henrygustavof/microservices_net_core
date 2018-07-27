16200be72c07ec38afd611a8ba8187ae56cdd912
henrygustavo <henrygustavof@gmail.com>
2018-07-26 20:35:18 -0500

    ini

---
+++ ShoesOnContainers/src/Services/ProductCatalogApi/Domain/CatalogItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogApi.Domain
+{
+    public class CatalogItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureFileName { get; set; }
+        public int PictureUrl { get; set; }
+    }
+}
