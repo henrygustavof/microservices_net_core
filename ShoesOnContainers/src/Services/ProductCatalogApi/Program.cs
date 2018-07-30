be1b55b762f4464df54994d10d18aca36c67d254
henrygustavo <henrygustavof@gmail.com>
2018-07-29 21:14:38 -0500

    catalog seed

--- ShoesOnContainers/src/Services/ProductCatalogApi/Program.cs
+++ ShoesOnContainers/src/Services/ProductCatalogApi/Program.cs
@@ -6,15 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ProductCatalogApi.Data;
 
 namespace ProductCatalogApi
 {
     public class Program
     {
         public static void Main(string[] args)
+
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<CatalogContext>();
+                    CatalogSeed.SeedAsync(context).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured while seeding the database");
+                }
+            }
+            host.Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
